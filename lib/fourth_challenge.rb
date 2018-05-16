5662f856316346773f3ac0f125aaa543453202ab
Mary-Kate James <marykatj@gmail.com>
2018-05-16 17:24:19 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
         {name: "Benvolio", age: "17", attitude: "worried"},
         {name: "Steven", age: "30", attitude: "confused"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -20,6 +20,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
